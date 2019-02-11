7653ad0b9b50deeed4f70edae672feb4798eec03
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-02-11 17:05:52 +0100

    Initial commit

---
+++ src/main/java/com/supergroup/reddot/ReddotApplication.java
@@ -0,0 +1,14 @@
+package com.supergroup.reddot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReddotApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/supergroup/reddot/ReddotApplicationTests.java
@@ -0,0 +1,17 @@
+package com.supergroup.reddot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReddotApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
